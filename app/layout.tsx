d60a0df0bf3ee6b56c55f071f6d3557b11a5a6bd
SplitandGO <splitandgo0@gmail.com>
2025-06-10 19:41:10 -0400

    fix: Actualizar rutas de logos en layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,11 +22,11 @@ export default function RootLayout({
         <ChakraProvider>
           <VStack minH="100vh" spacing={0} bg="gray.50">
             <Box as="header" w="100%" py={6} align="center">
-              <Image src="/logo-splitgo.png" alt="Split&Go" width={120} height={120} />
+              <Image src="/By Lefco Morada.png" alt="Split&Go" width={120} height={120} />
               <Text fontSize="2xl" fontWeight="bold" mt={2}>Split&Go Work</Text>
               <Box display="flex" alignItems="center" mt={1}>
                 <Text fontSize="md" color="gray.500" mr={2}>by</Text>
-                <Image src="/logo-lefko.png" alt="Lefko" width={60} height={24} />
+                <Image src="/Lefko 2.png" alt="Lefko" width={60} height={24} />
               </Box>
             </Box>
             <Box as="main" flex="1" w="100%">
