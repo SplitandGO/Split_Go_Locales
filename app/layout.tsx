114ab6c239dded0804a58a330e632b20209d0e72
SplitandGO <splitandgo0@gmail.com>
2025-06-10 19:37:03 -0400

    feat: Añadido branding Split&Go Work y by Lefko en el layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Image from 'next/image'
+import { Box, Text, VStack } from '@chakra-ui/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +20,19 @@ export default function RootLayout({
     <html lang="es">
       <body className={inter.className}>
         <ChakraProvider>
-          {children}
+          <VStack minH="100vh" spacing={0} bg="gray.50">
+            <Box as="header" w="100%" py={6} align="center">
+              <Image src="/logo-splitgo.png" alt="Split&Go" width={120} height={120} />
+              <Text fontSize="2xl" fontWeight="bold" mt={2}>Split&Go Work</Text>
+              <Box display="flex" alignItems="center" mt={1}>
+                <Text fontSize="md" color="gray.500" mr={2}>by</Text>
+                <Image src="/logo-lefko.png" alt="Lefko" width={60} height={24} />
+              </Box>
+            </Box>
+            <Box as="main" flex="1" w="100%">
+              {children}
+            </Box>
+          </VStack>
         </ChakraProvider>
       </body>
     </html>
